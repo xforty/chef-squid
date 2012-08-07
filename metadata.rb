029ffb38590b7fe063b863c1cddb05d7bfaf650d
David King <dking@xforty.com>
2012-08-07 13:05:17 -0400

    Prep for 1.0.0 release
    - Add changelog
    - Update version
    - Update readme

--- metadata.rb
+++ metadata.rb
@@ -3,4 +3,4 @@ maintainer_email "pearcec@xforty.com"
 license          "All rights reserved"
 description      "Installs/Configures squid"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "1.0.0"
